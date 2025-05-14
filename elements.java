aece2d6ff57627dbd8f6331e73b518e2cacb8ca0
vamsh-07 <vamsivamsh192@gmail.com>
2025-05-14 19:57:58 +0530

    Create elements.java

---
+++ elements.java
@@ -0,0 +1,9 @@
+public class Element {
+    public int atomicNumber;
+    public String symbol;
+    public String name;
+    public String atomicMass;
+    public int group;
+    public int period;
+    public String category;
+}
